7a80108600a8afa2d0d8117bebbf234d3a52e00d
Hyunsung Joo <joocurtis@gmail.com>
2025-04-09 13:33:14 +0900

    20250409

--- java-baekjoon/Bj2675.java
+++ java-baekjoon/Bj2675.java
@@ -4,18 +4,17 @@ import java.util.*;
 public class Bj2675 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
 
         int testNum = sc.nextInt();
-        int repeatNum = sc.nextInt();
-        String testCase = sc.nextLine();
+        
+        for(int i = 0; i < testNum; i++){
+            int repeatNum = sc.nextInt();
+            String sentence = sc.next();
+        }
 
-        String result = "";
+        for(int i = 0; i < repeatNum; i++){
 
-        for(int i = 0; i < testCase.length(); i++){
-            for(int j = 0; j < testNum; j++){
-                result += tesctCase.chartAt(i);
-            }
-            System.out.println(result);
         }
     }
 }
