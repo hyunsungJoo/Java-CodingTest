80dec61978b7eb922fd58c02ae247734864ce1a9
Hyunsung Joo <joocurtis@gmail.com>
2025-12-24 14:52:23 +0900

    [2025/12/24]

--- java-baekjoon/class2/Bj2231.java
+++ java-baekjoon/class2/Bj2231.java
@@ -1,10 +1,11 @@
-// 분배합
+// 분배합 2025/12/23
 import java.util.*;
 
 public class Bj2231 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+        num = sc.nextInt();
 
     }  
 }
---
+++ java-baekjoon/class2/Bj2292.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Bj2292 {
+    public static void main(String args{}){
+
+        int num = sc.nextInt();
+
+        for(int i = 1; i)
+    }
+}
