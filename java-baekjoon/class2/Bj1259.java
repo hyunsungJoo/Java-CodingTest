5095cab7e93320e1cc4a68b63ec2d7ca5319c424
Hyunsung Joo <joocurtis@gmail.com>
2026-01-14 08:49:59 +0900

    1259

--- java-baekjoon/class2/Bj1259.java
+++ java-baekjoon/class2/Bj1259.java
@@ -4,15 +4,20 @@ public class Bj1259 {
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
-
-        StringBuffer sb = new StringBuffer(sc.next());
         
 
         while(true){
-            sb = new StringBuffer(sc.next());
+            StringBuffer sb = new StringBuffer(sc.next());
             if(sb.toString().equals("0")) break;
-            if(sb == sb.reverse()) System.out.println("yes");
-            else System.out.println("no");
+
+            StringBuffer rv = sb;
+
+            if(sb.toString().equals(rv.reverse().toString())) {
+                System.out.println("yes");
+            }
+            else {
+                System.out.println("no");
+            }
         }
     }
 }
