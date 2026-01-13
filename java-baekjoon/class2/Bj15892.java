43dbeb2bb19d8aecac976f33fb086e90bca5c125
Hyunsung Joo <joocurtis@gmail.com>
2026-01-13 15:49:28 +0900

    260113

--- java-baekjoon/class2/Bj15892.java
+++ java-baekjoon/class2/Bj15892.java
@@ -6,9 +6,12 @@ public class Bj15892 {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         String s = sc.next();
+        int answer = 0;
 
         for(int i = 0; i < s.length(); i++){
-            
+            answer += (s.charAt(i) - 96) * (int)Math.pow(31, i);
         }
+
+        System.out.println(answer);
     }
 }
