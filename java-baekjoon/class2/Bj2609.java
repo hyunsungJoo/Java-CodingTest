1892a604b97a4404f13be4ed8d0042606562d186
Hyunsung Joo <joocurtis@gmail.com>
2026-01-15 16:17:01 +0900

    2775

--- java-baekjoon/class2/Bj2609.java
+++ java-baekjoon/class2/Bj2609.java
@@ -1,3 +1,4 @@
+// 최대공약수와 최소공배수
 import java.util.*;
 import java.io.*;
 
---
+++ java-baekjoon/class2/Bj2775.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Bj2775 {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStrea)
+
+    }
+}
