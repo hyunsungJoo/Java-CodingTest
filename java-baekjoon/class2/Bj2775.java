3dcd3b931111942d384a791a1d3fb3dfd482e2f0
Hyunsung Joo <joocurtis@gmail.com>
2026-02-11 15:52:51 +0900

    2775

--- java-baekjoon/class2/Bj2775.java
+++ java-baekjoon/class2/Bj2775.java
@@ -4,7 +4,32 @@ import java.io.*;
 public class Bj2775 {
     public static void main(String args[]) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStrea)
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(br.readLine());
+
+        int[][] personNum = new int[14][14];
+        int total = 0;
+
+        for(int i = 0; i < personNum[0].length; i++){
+            personNum[0][i] = i + 1;
+        }
+
+        for(int i = 0; i < personNum.length - 1; i++){
+            for(int j = 0; j < personNum[0].length; j++){
+                total += personNum[i][j];
+                personNum[i + 1][j] = total;
+            }
+            total = 0;
+        }
+        
+        for(int i = 0; i < count; i++){
+            int floor = Integer.parseInt(br.readLine());
+            int roomNum = Integer.parseInt(br.readLine());
+            System.out.println(personNum[floor][roomNum - 1]);
+        }
+
+
 
     }
 }
